ed7c5d632c16bbddcdf66e584e5cb9f692275196
Vikash <119168528+vikash0p@users.noreply.github.com>
2024-05-14 10:20:26 +0530

    update the data for user

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -7,13 +7,13 @@ const User = () => {
   console.log("🚀 ~ file: page.tsx:7 ~ user:", user);
   useEffect(() => {
     getUserData();
-  }, []);
+  }, [getUserData]);
 
   return (
-    <div className="w-full min-h-screen flex flex-col justify-center items-center text-center text-black">
-        <h1 className=' text-black'>{user?.user?._id} </h1>
-        <h1 className=' text-black'>{user?.user?.fullName} </h1>
-        <h1 className=' text-black'>{user?.user?.email} </h1>
+    <div className="flex flex-col items-center justify-center w-full min-h-screen text-center text-black">
+        <h1 className='text-black '>{user?.user?._id} </h1>
+        <h1 className='text-black '>{user?.user?.fullName} </h1>
+        <h1 className='text-black '>{user?.user?.email} </h1>
     </div>
   );
 };
