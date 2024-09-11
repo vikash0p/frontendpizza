2587fb9ad52a5ef443a8fe0bd54f10afd8d111f7
Vikash <119168528+vikash0p@users.noreply.github.com>
2024-09-11 20:39:52 +0530

    solve the fist error

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -11,7 +11,7 @@ const User = () => {
 
   return (
     <div className="w-full min-h-screen flex flex-col justify-center items-center text-center text-black">
-      <h1>User Details</h1>
+      <h1>User all Details</h1>
         <h1 className=' text-black'>{user?.user?._id} </h1>
         <h1 className=' text-black'>{user?.user?.fullName} </h1>
         <h1 className=' text-black'>{user?.user?.email} </h1>
